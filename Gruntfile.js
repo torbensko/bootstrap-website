5a3f2e622879b6749e4b9533264588e6335f6c34
Torben Sko <torben.sko@gmail.com>
2016-08-06 12:13:28 +1000

    Imported the Bootstrap theme, ready for modification

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  var pkgConfig = grunt.file.readJSON('package.json');
+
+  require('jit-grunt')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    'pkg': pkgConfig,
+
+    watch: {
+      sass: {
+        files: ['scss/*.{scss,sass}'],
+        tasks: ['sass:dev']
+      },
+      bower: {
+        files: ['bower.json'],
+        tasks: ['wiredep']
+      },
+    },
+    // Build CSS
+    sass: {
+      options: {
+        outputStyle: 'nested',
+        includePaths: ['./bower_components'],
+      },
+      dev: {
+        files: {
+          'styles/demo.css': 'scss/demo.scss'
+        }
+      },
+      dist: {
+        files: {
+          'styles/theme.css': 'scss/theme.scss'
+        }
+      }
+    },
+    // Inject dependencies
+    wiredep: {
+      options: {
+        devDependencies: true,
+      },
+      app: {
+        src: ['index.html'],
+        // ignorePath: '',
+      },
+      sass: {
+        src: ['./scss/*.{scss,sass}'],
+        ignorePath: '../bower_components/',
+        // exclude: [],
+      },
+    },
+  });
+
+  // Default task(s).
+  grunt.registerTask('default', [
+  		'sass:dev',
+  		'watch']);
+};
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Bootstrap
+
+A modified and stripped back bootstrap theme.
+
+
+## Usage
+
+```
+bower install torbensko/bootstrap-website --save
+```
+
+## Development
+
+- Build demo CSS
+	```
+	grunt
+	```
+
+- View sample theme (requires Python):
+	```
+	./run.sh
+	```
